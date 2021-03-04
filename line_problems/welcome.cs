59584e67f39997f89de0d56bc655029438f562bd
shivamunnooru <munnoorushiva07@gmail.com>
2021-03-04 16:25:06 +0530

    welcome message

---
+++ line_problems/welcome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace line_comparision_problems
+{
+    class welcome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line problems")
+        }
+    }
+}
