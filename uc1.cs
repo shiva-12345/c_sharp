131e9b32dd52c33d418cb8465484764c178ac805
shivamunnooru <munnoorushiva07@gmail.com>
2021-03-04 14:10:24 +0530

    [shiva]: emp wage usecase1

---
+++ uc1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace emp_present_ornot
+{
+    public class uc1
+    {
+        public static void employee_attendence()
+        {
+            int is_full_time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == is_full_time)
+                Console.WriteLine("employee is present");
+            else
+                Console.WriteLine("employee is not present");
+        }
+    }
+}
