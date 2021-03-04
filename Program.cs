0b4c11b54267064348624e456d4b27d43392e1f0
shivamunnooru <munnoorushiva07@gmail.com>
2021-03-04 11:15:18 +0530

    added files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace emp_present_ornot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class1.partimeempwage();
+            uc4.partimeempwageswitchcase();
+            Console.ReadKey();
+        }
+    }
+}
+
+        
---
+++ uc4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace emp_present_ornot
+{
+    class uc4
+    {
+        public const int is_full_time = 1;
+        public const int is_part_time = 2;
+        public const int emp_rate_per_hour = 20;
+        public static void partimeempwageswitchcase()
+        {
+           
+            int emphrs = 0;
+            int empwage = 0;            
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
+            {
+                case is_part_time:
+                    emphrs = 4;
+                    break;
+                case is_full_time:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+
+            }
+            empwage = emphrs * emp_rate_per_hour;
+            Console.WriteLine("emp wage" + empwage);
+            Console.ReadKey();
+
+
+        }
+    }
+
+} 
